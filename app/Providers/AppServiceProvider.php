1b95072831211ae11a74d71c4d661bcfdc3f547f
macist-m <info@macist.net>
2022-10-31 19:56:21 +0300

    fixed https error on app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if (env('APP_ENV') !== 'local') {
-            $url->forceSchema('https');
-        }
+        $url->forceScheme('https');
     }
 }
